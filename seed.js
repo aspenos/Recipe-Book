91163b3ea8ea9df431e42bf730e2c38423589d80
aspenos <arisspenos1@gmail.com>
2024-04-09 22:48:44 -0400

    fetch allergens and ingredientCategories

--- seed.js
+++ seed.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Allergen from './models/allergen.js';
+import Ingredient from './models/ingredient.js';
 import IngredientCategory from './models/ingredientCategory.js';
+
 
 import allergens from './data/allergens.json' assert {type: 'json'};
 import ingredientCategories from './data/ingredientCategories.json' assert {type: 'json'};
+import ingredients from './data/ingredients.json' assert {type: 'json'};
 
 dotenv.config();
 
@@ -12,14 +15,21 @@ const seedDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected...');
-
-        
         await Allergen.deleteMany({});
         await IngredientCategory.deleteMany({});
-
-        
+        await Ingredient.deleteMany({});
         await Allergen.insertMany(allergens);
         await IngredientCategory.insertMany(ingredientCategories);
+
+        const dbIngredientCategories = await IngredientCategory.find();
+        
+        const dbAllergens = await Allergen.find();
+
+        let categoryMap = {};
+        dbIngredientCategories.forEach(cat => categoryMap[cat.name] = cat._id);
+
+        let allergenMap = {};
+        dbAllergens.forEach(all => allergenMap[all.name] = all._id);
 
         console.log('Ingredient categories and allergens seeded successfully');
     } catch (err) {
