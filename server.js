2742f89c08c42b69799ab0fb892f08e50574c154
aspenos <arisspenos1@gmail.com>
2024-04-14 19:11:00 -0400

    Test basic server setup

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-import 'dotenv/config.js'
-import './config/database.js'
 import express from 'express';
-import recipeRoutes from './routes/recipeRoutes.js';
-import ingredientRoutes from './routes/ingredientRoutes.js';
-import ingredientCategoryRoutes from './routes/ingredientCategoryRoutes.js';
-import recipeCategoryRoutes from './routes/recipeCategoryRoutes.js';
-import allergenRoutes from './routes/allergenRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import verifyToken from './middleware/authMiddleware.js';
-import cors from 'cors'
-
-const server = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log(`Your MongoDB URI is: ${process.env.MONGO_URI}`);
+app.get('/', (req, res) => {
+  res.send('Hello, world! The server is working!');
+});
 
-server.use(express.json());
-server.use(cors());
-
-server.use('/recipes', recipeRoutes);
-server.use('/ingredients', ingredientRoutes);
-server.use('/ingredient-categories', ingredientCategoryRoutes);
-server.use('/recipe-categories', recipeCategoryRoutes);
-server.use('/allergens', allergenRoutes);
-server.use('/users', userRoutes);
-
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
